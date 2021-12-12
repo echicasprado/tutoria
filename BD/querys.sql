f196a5685e2cf24a8e3940dfb0820f72f3963ffb
Ever <everchicasp@outlook.com>
2021-12-11 21:26:36 -0600

    update query

--- BD/querys.sql
+++ BD/querys.sql
@@ -1,5 +1,7 @@
 -- Obtener la informacion de los clientes
-select * from Clientes, Personas, TiposClientes, Generos WHERE Clientes.persona = Personas.persona AND Clientes.tipo_cliente = TiposClientes.tipo_cliente AND Personas.genero = Generos.genero;
+SELECT P.nombre, P.dpi, P.nit, P.telefono, TC.nombre AS TIPO_CLIENTE, G.nombre AS GENERO 
+FROM Clientes AS C, Personas AS P, TiposClientes AS TC, Generos AS G 
+WHERE C.persona = P.persona AND C.tipo_cliente = TC.tipo_cliente AND P.genero = G.genero;
 
 -- Colaboradores
 select * from Colaboradores, TiposColaboradores WHERE Colaboradores.tipo_colaborador = TiposColaboradores.tipo_colaborador ORDER BY colaborador;
